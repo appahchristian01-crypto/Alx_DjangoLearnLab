50a1e5526c32d92b04b6ce1eabbaf94192481df6
appahchristian01-crypto <appahchristian01@gmail.com>
2025-11-08 14:10:19 +0000

    urls.py

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import SignUpView
+from . import views
 
 urlpatterns = [
-    path('signup/', SignUpView.as_view(), name='signup'),
+    path('books/', views.list_books, name='list_books'),  # Function-based view
+    path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),  # Class-based view
 ]
