82091bbb20676abf74d8d944878880df08ae026f
appahchristian01-crypto <appahchristian01@gmail.com>
2025-11-08 01:08:13 +0000

    views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,8 +1,18 @@
 from django.contrib.auth.forms import UserCreationForm
-from django.urls import reverse_lazy
-from django.views.generic import CreateView
+from django. urls import reverse_lazy
+from django. views.generic import CreateView
 
 class SignUpView(CreateView):
     form_class = UserCreationForm  # This is the built-in Django signup form
     success_url = reverse_lazy("login")  # After signing up, go to login page
     template_name = "registration/signup.html"  # Which template to use
+
+from django.shortcuts import render
+from .models import Book
+
+def list_books(request):
+    """Display all books with their authors"""
+    books = Book.objects.all()  # Get all Book records from the database
+    context = {'books': books}
+    return render(request, 'relationship_app/list_books.html', context)
+
