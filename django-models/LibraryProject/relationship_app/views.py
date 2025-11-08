7134a16059c40aa407f4f4a2a6e9d7f4a11ef823
appahchristian01-crypto <appahchristian01@gmail.com>
2025-11-08 01:10:18 +0000

    views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -16,3 +16,13 @@ def list_books(request):
     context = {'books': books}
     return render(request, 'relationship_app/list_books.html', context)
 
+from django.shortcuts import render
+from django.views.generic import DetailView
+from .models import Book, Library  # Make sure Library is imported
+
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = 'relationship_app/library_detail.html'
+    context_object_name = 'library'  # this will be used in the template
+
+
