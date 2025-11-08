aaeeb4b9cea6f3e9d61b3f65db87cde0c0d9561e
appahchristian01-crypto <appahchristian01@gmail.com>
2025-11-08 01:01:42 +0000

    Delete django-models directory

--- django-models/bookshelf/admin.py
+++
@@ -1,9 +0,0 @@
-<<<<<<< HEAD
-from django.contrib import admin
-
-# Register your models here.
-=======
-from django.contrib import admin
-
-# Register your models here.
->>>>>>> b432710 (Add django-models project with relationship_app)
