dd8a6dd75d9f9612267c3b0088e7d60a7adfaa57
appahchristian01-crypto <appahchristian01@gmail.com>
2025-11-08 01:13:29 +0000

    views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,28 +1,17 @@
-from django.contrib.auth.forms import UserCreationForm
-from django. urls import reverse_lazy
-from django. views.generic import CreateView
+# LibraryProject/relationship_app/views.py
 
-class SignUpView(CreateView):
-    form_class = UserCreationForm  # This is the built-in Django signup form
-    success_url = reverse_lazy("login")  # After signing up, go to login page
-    template_name = "registration/signup.html"  # Which template to use
-
-from django.shortcuts import render
-from .models import Book
-
-def list_books(request):
-    """Display all books with their authors"""
-    books = Book.objects.all()  # Get all Book records from the database
-    context = {'books': books}
-    return render(request, 'relationship_app/list_books.html', context)
-
+# Imports
 from django.shortcuts import render
 from django.views.generic import DetailView
-from .models import Book, Library  # Make sure Library is imported
+from .models import Book, Library
 
+# Function-Based View: List all books
+def list_books(request):
+    books = Book.objects.all()  # get all books from the database
+    return render(request, 'relationship_app/list_books.html', {'books': books})
+
+# Class-Based View: Show details for a specific library
 class LibraryDetailView(DetailView):
-    model = Library
-    template_name = 'relationship_app/library_detail.html'
-    context_object_name = 'library'  # this will be used in the template
-
-
+    model = Library                # the model to use
+    template_name = 'relationship_app/library_detail.html'  # template file
+    context_object_name = 'library'  # name to use in the template
