a932128a679087256e7e24b3d63664f42dc9f292
appahchristian01-crypto <appahchristian01@gmail.com>
2025-11-08 01:15:38 +0000

    models.py

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -1,28 +1,16 @@
 from django.db import models
 
 class Author(models.Model):
-    name = models.CharField(max_length=255)
-
-    def __str__(self):
-        return self.name
+    name = models.CharField(max_length=100)
 
 class Book(models.Model):
-    title = models.CharField(max_length=255)
-    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='books')
-
-    def __str__(self):
-        return self.title
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
 
 class Library(models.Model):
-    name = models.CharField(max_length=255)
-    books = models.ManyToManyField(Book, related_name='libraries')
-
-    def __str__(self):
-        return self.name
+    name = models.CharField(max_length=100)
+    books = models.ManyToManyField(Book)
 
 class Librarian(models.Model):
-    name = models.CharField(max_length=255)
-    library = models.OneToOneField(Library, on_delete=models.CASCADE, related_name='librarian')
-
-    def __str__(self):
-        return f"{self.name} (Librarian of {self.library.name})"
+    name = models.CharField(max_length=100)
+    library = models.OneToOneField(Library, on_delete=models.CASCADE)
