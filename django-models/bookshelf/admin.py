5d3758f60079bc75072c4a3b47ab70e616dd7830
appahchristian01-crypto <appahchristian01@gmail.com>
2025-11-08 00:52:40 +0000

    Add files via upload

---
+++ django-models/bookshelf/admin.py
@@ -0,0 +1,9 @@
+<<<<<<< HEAD
+from django.contrib import admin
+
+# Register your models here.
+=======
+from django.contrib import admin
+
+# Register your models here.
+>>>>>>> b432710 (Add django-models project with relationship_app)
---
+++ django-models/bookshelf/apps.py
@@ -0,0 +1,15 @@
+<<<<<<< HEAD
+from django.apps import AppConfig
+
+
+class BookshelfConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'bookshelf'
+=======
+from django.apps import AppConfig
+
+
+class BookshelfConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'bookshelf'
+>>>>>>> b432710 (Add django-models project with relationship_app)
