3d914a18a2d14cfab9aa0c65743d922327fb2bc9
appahchristian01-crypto <appahchristian01@gmail.com>
2025-11-08 01:18:01 +0000

    views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,17 +1,21 @@
 # LibraryProject/relationship_app/views.py
 
-# Imports
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views.generic import DetailView
 from .models import Book, Library
 
 # Function-Based View: List all books
 def list_books(request):
-    books = Book.objects.all()  # get all books from the database
+    books = Book.objects.all()  # Get all books from the database
     return render(request, 'relationship_app/list_books.html', {'books': books})
 
-# Class-Based View: Show details for a specific library
+# Class-Based View: Show library details
 class LibraryDetailView(DetailView):
-    model = Library                # the model to use
-    template_name = 'relationship_app/library_detail.html'  # template file
-    context_object_name = 'library'  # name to use in the template
+    model = Library
+    template_name = 'relationship_app/library_detail.html'
+    context_object_name = 'library'  # This variable is used in the template
+
+# Optional: Example of using get_object_or_404 (if you want to use a function-based detail view)
+def library_detail(request, pk):
+    library = get_object_or_404(Library, pk=pk)
+    return render(request, 'relationship_app/library_detail.html', {'library': library})
