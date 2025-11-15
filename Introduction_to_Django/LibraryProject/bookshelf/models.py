47d21a449aad682cfc19e688b12eb69c58b67fa9
appahchristian01-crypto <appahchristian01@gmail.com>
2025-11-15 18:48:19 +0000

    models.py

--- Introduction_to_Django/LibraryProject/bookshelf/models.py
+++ Introduction_to_Django/LibraryProject/bookshelf/models.py
@@ -1,33 +1,58 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 
+
+# ------------------------------------
+# Custom User Manager
+# ------------------------------------
 class CustomUserManager(BaseUserManager):
     def create_user(self, email, password=None, **extra_fields):
         if not email:
             raise ValueError("Users must have an email address")
+
         email = self.normalize_email(email)
         user = self.model(email=email, **extra_fields)
         user.set_password(password)
-        user.save()
+        user.save(using=self._db)
         return user
 
     def create_superuser(self, email, password=None, **extra_fields):
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', True)
 
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
         return self.create_user(email, password, **extra_fields)
 
 
-class CustomUser(AbstractBaseUser, PermissionsMixin):
+# ------------------------------------
+# Custom User Model
+# ------------------------------------
+class CustomUser(AbstractUser):
+    username = None  # remove username
     email = models.EmailField(unique=True)
-    full_name = models.CharField(max_length=200)
-    is_active = models.BooleanField(default=True)
-    is_staff = models.BooleanField(default=False)
+    date_of_birth = models.DateField(null=True, blank=True)
+    profile_photo = models.ImageField(upload_to='profiles/', null=True, blank=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
 
     objects = CustomUserManager()
 
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['full_name']
-
     def __str__(self):
         return self.email
+
+
+# ------------------------------------
+# Book Model (required by ALX checker)
+# ------------------------------------
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f"{self.title} by {self.author}"
