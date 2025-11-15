bd4f551012741af2297e462faccad3744e89ce97
appahchristian01-crypto <appahchristian01@gmail.com>
2025-11-15 20:40:43 +0000

    Add files via upload

---
+++ advanced_features_and_security/LibraryProject/accounts/models.py
@@ -0,0 +1,53 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser, BaseUserManager
+from django.utils.translation import gettext_lazy as _
+
+class CustomUserManager(BaseUserManager):
+    """
+    Custom manager to create users and superusers.
+    We keep username as the unique identifier (like default).
+    """
+    use_in_migrations = True
+
+    def _create_user(self, username, email, password, **extra_fields):
+        if not username:
+            raise ValueError('The given username must be set')
+        email = self.normalize_email(email)
+        user = self.model(username=username, email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, username, email=None, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', False)
+        extra_fields.setdefault('is_superuser', False)
+        return self._create_user(username, email, password, **extra_fields)
+
+    def create_superuser(self, username, email=None, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self._create_user(username, email, password, **extra_fields)
+
+
+class CustomUser(AbstractUser):
+    """
+    Extend Django's AbstractUser by adding:
+      - date_of_birth: Date
+      - profile_photo: Image
+    """
+    date_of_birth = models.DateField(_('date of birth'), null=True, blank=True)
+    profile_photo = models.ImageField(_('profile photo'),
+                                      upload_to='profile_photos/',
+                                      null=True,
+                                      blank=True)
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.username
---
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -0,0 +1,9 @@
+<<<<<<< HEAD
+from django.shortcuts import render
+
+# Create your views here.
+=======
+from django.shortcuts import render
+
+# Create your views here.
+>>>>>>> b432710 (Add django-models project with relationship_app)
