97ae87ab63e4425b35c03c507339635e1dd39a2e
appahchristian01-crypto <appahchristian01@gmail.com>
2025-11-15 18:53:45 +0000

    admin.py

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -1,29 +1,40 @@
 from django.contrib import admin
+from .models import Book, CustomUser
 from django.contrib.auth.admin import UserAdmin
-from .models import CustomUser
 
 
+# -----------------------------
+# Book Model Admin
+# -----------------------------
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ("title", "author")
+    search_fields = ("title", "author")
+    list_filter = ("author",)
+
+
+# -----------------------------
+# Custom User Admin
+# -----------------------------
 class CustomUserAdmin(UserAdmin):
     model = CustomUser
-    list_display = ('email', 'full_name', 'is_staff', 'is_active')
-    list_filter = ('is_staff', 'is_active')
-    ordering = ('email',)
+    list_display = ("email", "date_of_birth", "is_staff", "is_active")
+    list_filter = ("is_staff", "is_active")
+    search_fields = ("email",)
+    ordering = ("email",)
 
     fieldsets = (
-        (None, {'fields': ('email', 'password')}),
-        ('Personal Info', {'fields': ('full_name',)}),
-        ('Permissions', {'fields': ('is_staff', 'is_active', 'is_superuser', 'groups', 'user_permissions')}),
-        ('Important dates', {'fields': ('last_login',)}),
+        (None, {"fields": ("email", "password")}),
+        ("Personal Info", {"fields": ("date_of_birth", "profile_photo")}),
+        ("Permissions", {"fields": ("is_staff", "is_active", "is_superuser", "groups", "user_permissions")}),
     )
 
     add_fieldsets = (
         (None, {
-            'classes': ('wide',),
-            'fields': ('email', 'full_name', 'password1', 'password2', 'is_staff', 'is_active')
+            "classes": ("wide",),
+            "fields": ("email", "password1", "password2", "is_staff", "is_active"),
         }),
     )
-
-    search_fields = ('email', 'full_name')
 
 
 admin.site.register(CustomUser, CustomUserAdmin)
