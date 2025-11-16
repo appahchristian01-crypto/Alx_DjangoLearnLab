b93c0e9bfcef3fd61c7d214fb1a14e9e34db3377
appahchristian01-crypto <appahchristian01@gmail.com>
2025-11-16 22:40:11 +0000

    Delete advanced_features_and_security/LibraryProject directory

--- advanced_features_and_security/LibraryProject/bookshelf/tests.py
+++
@@ -1,9 +0,0 @@
-<<<<<<< HEAD
-from django.test import TestCase
-
-# Create your tests here.
-=======
-from django.test import TestCase
-
-# Create your tests here.
->>>>>>> b432710 (Add django-models project with relationship_app)
--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++
@@ -1,9 +0,0 @@
-<<<<<<< HEAD
-from django.shortcuts import render
-
-# Create your views here.
-=======
-from django.shortcuts import render
-
-# Create your views here.
->>>>>>> b432710 (Add django-models project with relationship_app)
