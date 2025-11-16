3a477c5366a8ba8cb8558308daae5c8662213d16
appahchristian01-crypto <appahchristian01@gmail.com>
2025-11-16 22:41:20 +0000

    Add files via upload

---
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+from django.contrib.auth.models import AbstractUser, BaseUserManager
+User = get_user_model()
+
+# Create your models here.
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=100)
+    publication_year = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.title} by {self.author}"
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, username, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError("Email is required")
+        email = self.normalize_email(email)
+        user = self.model(username=username, email=email, **extra_fields)
+        user.set_password(password)
+        user.save()
+        return user
+    
