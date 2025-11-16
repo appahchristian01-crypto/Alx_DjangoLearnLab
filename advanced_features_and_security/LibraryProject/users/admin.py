b7f6c52fa406a6ed4f228d5b5595cdc04a9d8c78
appahchristian01-crypto <appahchristian01@gmail.com>
2025-11-16 23:05:11 +0000

    admin.py

--- advanced_features_and_security/LibraryProject/users/admin.py
+++ advanced_features_and_security/LibraryProject/users/admin.py
@@ -1,14 +1,16 @@
-# Register your models here.
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import CustomUser
 
-class CustomUserAdmin(UserAdmin):
-    model = CustomUser
-    list_display = ('username', 'email', 'date_of_birth', 'is_staff')
 
+class CustomUserAdmin(UserAdmin):
     fieldsets = UserAdmin.fieldsets + (
-        ('Additional Info', {'fields': ('date_of_birth', 'profile_photo')}),
+        ("Additional Information", {"fields": ("date_of_birth", "profile_photo")}),
     )
+
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ("Additional Information", {"fields": ("date_of_birth", "profile_photo")}),
+    )
+
 
 admin.site.register(CustomUser, CustomUserAdmin)
