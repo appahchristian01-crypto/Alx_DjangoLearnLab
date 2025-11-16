f92e514cbe0614fea45c6acb881b0fe7d614d993
appahchristian01-crypto <appahchristian01@gmail.com>
2025-11-16 22:53:45 +0000

    admin.py

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,12 +1,29 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser, Book
 
-# Register your models here.
 
-from .models import Book  # import the Book model
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    list_display = ("email", "date_of_birth", "is_staff", "is_active")
+    list_filter = ("is_staff", "is_active")
 
-# Register Book model with custom admin display
-@admin.register(Book)
-class BookAdmin(admin.ModelAdmin):
-    list_display = ('title', 'author', 'publication_year')  # columns to show
-    list_filter = ('publication_year', 'author')  # filters on the right side
-    search_fields = ('title', 'author')  # search bar at the top
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        ("Personal info", {"fields": ("date_of_birth", "profile_photo")}),
+        ("Permissions", {"fields": ("is_staff", "is_active", "is_superuser", "groups", "user_permissions")}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            "classes": ("wide",),
+            "fields": ("email", "password1", "password2", "is_staff", "is_active"),
+        }),
+    )
+
+    search_fields = ("email",)
+    ordering = ("email",)
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
+admin.site.register(Book)
