fa08ea82911347b80ccfa0da3d49df56fad54801
appahchristian01-crypto <appahchristian01@gmail.com>
2025-11-16 23:18:35 +0000

    models.py

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,63 +1,58 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser, BaseUserManager
-from django.utils import timezone
 
 
 # -------------------------
 # Custom User Manager
 # -------------------------
 class CustomUserManager(BaseUserManager):
-    def create_user(self, email, password=None, **extra_fields):
-        if not email:
-            raise ValueError("Users must have an email address")
+    def create_user(self, username, email=None, password=None, **extra_fields):
+        if not username:
+            raise ValueError("Users must have a username")
 
         email = self.normalize_email(email)
-        user = self.model(email=email, **extra_fields)
+        user = self.model(username=username, email=email, **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
         return user
 
-    def create_superuser(self, email, password=None, **extra_fields):
-        extra_fields.setdefault('is_staff', True)
-        extra_fields.setdefault('is_superuser', True)
+    def create_superuser(self, username, email=None, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
 
-        return self.create_user(email, password, **extra_fields)
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True.")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True.")
+
+        return self.create_user(username, email, password, **extra_fields)
 
 
 # -------------------------
 # Custom User Model
 # -------------------------
 class CustomUser(AbstractUser):
-    username = None  # we use email instead
-    email = models.EmailField(unique=True)
     date_of_birth = models.DateField(null=True, blank=True)
-    profile_photo = models.ImageField(upload_to='profile_photos/', null=True, blank=True)
-    date_joined = models.DateTimeField(default=timezone.now)
-
-    USERNAME_FIELD = "email"
-    REQUIRED_FIELDS = []
+    profile_photo = models.ImageField(upload_to="profiles/", null=True, blank=True)
 
     objects = CustomUserManager()
 
     def __str__(self):
-        return self.email
+        return self.username
 
 
 # -------------------------
-# Book Model + Permissions
+# Book Model + Custom Permissions
 # -------------------------
 class Book(models.Model):
     title = models.CharField(max_length=200)
-    author = models.CharField(max_length=150)
-    published_date = models.DateField(null=True, blank=True)
-    description = models.TextField(blank=True)
+    author = models.CharField(max_length=200)
+    publication_year = models.IntegerField()
 
     class Meta:
         permissions = [
-            ("can_create", "Can create a book"),
-            ("can_delete", "Can delete a book"),
-            ("can_edit", "Can edit a book"),
-            ("can_view", "Can view a book"),
+            ("can_create", "Can create books"),
+            ("can_delete", "Can delete books"),
         ]
 
     def __str__(self):
