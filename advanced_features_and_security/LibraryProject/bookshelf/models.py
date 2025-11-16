c586fd2f56e9f00d96c49d99a1c2e292ee6fb739
appahchristian01-crypto <appahchristian01@gmail.com>
2025-11-16 22:50:41 +0000

    models.py

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,5 +1,14 @@
 from django.db import models
-from django.conf import settings
+from django.contrib.auth.models import AbstractUser
+from django.utils import timezone
+
+
+class CustomUser(AbstractUser):
+    date_of_birth = models.DateField(null=True, blank=True)
+    profile_photo = models.ImageField(upload_to='profile_photos/', null=True, blank=True)
+
+    def __str__(self):
+        return self.username
 
 
 class Book(models.Model):
