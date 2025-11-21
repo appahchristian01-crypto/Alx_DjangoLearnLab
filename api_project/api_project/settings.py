11dd9e31c086f6347c33dd4a3ee0570cd3934b61
appahchristian01-crypto <appahchristian01@gmail.com>
2025-11-21 21:33:12 +0000

    settings.py

--- api_project/api_project/settings.py
+++ api_project/api_project/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'api',
+    'rest_framework.authtoken',
+
 
 
 ]
@@ -124,3 +126,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
