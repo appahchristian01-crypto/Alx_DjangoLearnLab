66369ba0f0bd58dd3c8c24f294213006275f61e7
appahchristian01-crypto <appahchristian01@gmail.com>
2025-11-21 21:42:40 +0000

    settings.py

--- api_project/api_project/settings.py
+++ api_project/api_project/settings.py
@@ -37,12 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Third-party apps
     'rest_framework',
-    'api',
     'rest_framework.authtoken',
 
-
-
+    # Local apps
+    'api',
 ]
 
 MIDDLEWARE = [
@@ -122,8 +123,19 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+
+# -----------------------------
+# Django REST Framework settings
+# -----------------------------
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ]
+}
