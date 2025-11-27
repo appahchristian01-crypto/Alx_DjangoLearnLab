8d97ee072bc100169683d9bee4d15b8a8928876a
appahchristian01-crypto <appahchristian01@gmail.com>
2025-11-27 03:45:16 +0000

    urls.py

--- advanced-api-project/advanced_api_project/urls.py
+++ advanced-api-project/advanced_api_project/urls.py
@@ -1,7 +1,8 @@
+# advanced_project/urls.py
 from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('api.urls')),   # include your API URLs here
+    path('api/', include('api.urls')),   # Include API URLs
 ]
