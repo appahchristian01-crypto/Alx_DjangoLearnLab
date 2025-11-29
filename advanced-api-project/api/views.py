6fca43270a6b950151f63a4ae48eb99628dcb3ec
appahchristian01-crypto <appahchristian01@gmail.com>
2025-11-29 18:00:10 +0000

    views.py

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,4 +1,5 @@
 from rest_framework import generics, permissions, filters
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 from django_filters import rest_framework
 from django_filters.rest_framework import DjangoFilterBackend
 from .models import Book
@@ -37,4 +38,5 @@ class BookDeleteView(generics.DestroyAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_classes = [permissions.IsAuthenticated]
+
 
