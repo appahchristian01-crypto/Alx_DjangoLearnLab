68dd1a2e770a75588d18f0a16f66ed2988d8275c
appahchristian01-crypto <appahchristian01@gmail.com>
2025-11-29 17:23:33 +0000

    views.py

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,34 +1,24 @@
 from rest_framework import generics, permissions, filters
+from django_filters import rest_framework
 from django_filters.rest_framework import DjangoFilterBackend
 from .models import Book
 from .serializers import BookSerializer
 
 class BookListCreateView(generics.ListCreateAPIView):
-    """
-    GET: List all books (anyone can access)
-    POST: Create a new book (authenticated users only)
-    Supports filtering, ordering, and search
-    """
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
-    # Filters and search
-    filter_backends = [DjangoFilterBackend, filters.OrderingFilter, filters.SearchFilter]
-    filterset_fields = ['title', 'author', 'published_year']  # exact filtering
-    ordering_fields = ['title', 'published_year']             # allow ordering
-    search_fields = ['title', 'author__name']                 # allow search by title or author name
+    filter_backends = [rest_framework.DjangoFilterBackend, filters.OrderingFilter, filters.SearchFilter]
+    filterset_fields = ['title', 'author', 'published_year']
+    ordering_fields = ['title', 'published_year']
+    search_fields = ['title', 'author__name']
 
     def get_permissions(self):
         if self.request.method == 'POST':
             return [permissions.IsAuthenticated()]
         return [permissions.AllowAny()]
 
-
 class BookDetailView(generics.RetrieveUpdateDestroyAPIView):
-    """
-    GET: Retrieve book details (anyone can access)
-    PUT/PATCH/DELETE: Only authenticated users can modify or delete
-    """
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
