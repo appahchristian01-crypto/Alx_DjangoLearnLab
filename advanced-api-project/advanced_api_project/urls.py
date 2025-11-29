84e7f53ff47a6281710e8b8891a5d027ad64bfbd
appahchristian01-crypto <appahchristian01@gmail.com>
2025-11-29 16:40:07 +0000

    Delete advanced-api-project directory

--- advanced-api-project/advanced_api_project/urls.py
+++
@@ -1,8 +0,0 @@
-# advanced_project/urls.py
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include('api.urls')),   # Include API URLs
-]
