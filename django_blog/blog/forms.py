f5d8cf534c6f0c11359cdc78d006be2f73dbe901
appahchristian01-crypto <appahchristian01@gmail.com>
2025-12-06 08:23:32 -0800

    forms.py

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from .models import Post, Comment
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from taggit.forms import TagWidget   # <-- REQUIRED for tagging functionality
+from taggit.forms import TagWidget()   # <-- REQUIRED for tagging functionality
 
 
 # -----------------------------
@@ -45,3 +45,4 @@ class CommentForm(forms.ModelForm):
         if not content or len(content.strip()) == 0:
             raise forms.ValidationError("Comment cannot be empty.")
         return content
+
