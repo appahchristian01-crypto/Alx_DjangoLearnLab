fe2d779f88cda6a0856a7025d593e1b6ba82bd1c
appahchristian01-crypto <appahchristian01@gmail.com>
2025-12-06 06:41:06 -0800

    views.py

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,11 +1,56 @@
-from django.shortcuts import get_object_or_404
-from django.views.generic import CreateView, UpdateView, DeleteView
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from django.contrib.auth.models import User
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
-from .models import Comment, Post
-from .forms import CommentForm
+from .models import Post, Comment
+from .forms import PostForm, CommentForm, RegisterForm
 
 # -----------------------------
-# Create Comment
+# Post CRUD Views
+# -----------------------------
+class PostListView(ListView):
+    model = Post
+    template_name = 'post_list.html'
+    context_object_name = 'posts'
+    ordering = ['-id']
+    paginate_by = 5
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'post_detail.html'
+    context_object_name = 'post'
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    form_class = PostForm
+    template_name = 'post_form.html'
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    form_class = PostForm
+    template_name = 'post_form.html'
+
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    template_name = 'post_confirm_delete.html'
+    success_url = '/'
+
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author
+
+# -----------------------------
+# Comment CRUD Views
 # -----------------------------
 class CommentCreateView(LoginRequiredMixin, CreateView):
     model = Comment
@@ -14,16 +59,13 @@ class CommentCreateView(LoginRequiredMixin, CreateView):
 
     def form_valid(self, form):
         form.instance.author = self.request.user
-        post_id = self.kwargs.get('pk')  # expects URL like /post/<pk>/comments/new/
+        post_id = self.kwargs.get('pk')
         form.instance.post = get_object_or_404(Post, pk=post_id)
         return super().form_valid(form)
 
     def get_success_url(self):
         return self.object.post.get_absolute_url()
 
-# -----------------------------
-# Update Comment
-# -----------------------------
 class CommentUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Comment
     form_class = CommentForm
@@ -36,9 +78,6 @@ class CommentUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     def get_success_url(self):
         return self.object.post.get_absolute_url()
 
-# -----------------------------
-# Delete Comment
-# -----------------------------
 class CommentDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = Comment
     template_name = 'comment_confirm_delete.html'
@@ -49,3 +88,33 @@ class CommentDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
 
     def get_success_url(self):
         return self.object.post.get_absolute_url()
+
+# -----------------------------
+# User Registration
+# -----------------------------
+def register(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Account created successfully! You can now log in.')
+            return redirect('login')
+    else:
+        form = RegisterForm()
+    return render(request, 'register.html', {'form': form})
+
+# -----------------------------
+# Profile View
+# -----------------------------
+@login_required
+def profile(request):
+    user = request.user
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        user.username = username
+        user.email = email
+        user.save()
+        messages.success(request, "Your profile has been updated!")
+        return redirect('profile')
+    return render(request, 'profile.html', {'user': user})
