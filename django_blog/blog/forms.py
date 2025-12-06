6ab48cee22b864021e6c7b808be539827d2b7e46
appahchristian01-crypto <appahchristian01@gmail.com>
2025-12-06 06:08:22 -0800

    forms.py

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -3,23 +3,17 @@ from .models import Post, Comment
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
-
 # -----------------------------
 # Post Form
 # -----------------------------
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
-
-        # Use these fields ONLY if you have a Tag model added
-        # If you don’t have tags, remove 'tags'
-        fields = ['title', 'content']  
-
+        fields = ['title', 'content']  # Include 'tags' if you have a Tag model
         widgets = {
             'title': forms.TextInput(attrs={'placeholder': 'Enter title here'}),
             'content': forms.Textarea(attrs={'rows': 8, 'placeholder': 'Write your post...'}),
         }
-
 
 # -----------------------------
 # Comment Form
@@ -28,14 +22,11 @@ class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ['content']
+        widgets = {
+            'content': forms.Textarea(attrs={'rows': 3, 'placeholder': 'Write your comment here...'})
+        }
 
-
-# -----------------------------
-# User Registration Form
-# -----------------------------
-class RegisterForm(UserCreationForm):
-    email = forms.EmailField(required=True)
-
-    class Meta:
-        model = User
-        fields = ("username", "email", "password1", "password2")
+    def clean_content(self):
+        content = self.cleaned_data.get('content')
+        if not content or len(content.strip()) == 0:
+            raise forms.ValidationError
