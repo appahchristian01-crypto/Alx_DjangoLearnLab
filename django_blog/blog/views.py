3c2257a71510f91e5bfacadda741a30305e0897b
appahchristian01-crypto <appahchristian01@gmail.com>
2025-12-06 06:13:40 -0800

    views.py

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,96 +1,51 @@
-from django.shortcuts import render, redirect, get_object_or_404
-from django.urls import reverse_lazy
+from django.shortcuts import get_object_or_404
+from django.views.generic import CreateView, UpdateView, DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
-from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
-from django.contrib.auth.decorators import login_required
-from django.db.models import Q
-from .models import Post
-from .forms import PostForm, RegisterForm
+from .models import Comment, Post
+from .forms import CommentForm
 
-# ---------------------
-# AUTH VIEWS
-# ---------------------
-
-def register(request):
-    if request.method == "POST":
-        form = RegisterForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('login')
-    else:
-        form = RegisterForm()
-    return render(request, 'blog/register.html', {'form': form})
-
-
-@login_required
-def profile(request):
-    return render(request, 'blog/profile.html')
-
-
-# ---------------------
-# BLOG VIEWS
-# ---------------------
-
-class PostListView(ListView):
-    model = Post
-    template_name = 'blog/post_list.html'
-    context_object_name = 'posts'
-    ordering = ['-published_date']
-    paginate_by = 10
-
-
-class PostDetailView(DetailView):
-    model = Post
-    template_name = 'blog/post_detail.html'
-    context_object_name = 'post'
-
-
-class PostCreateView(LoginRequiredMixin, CreateView):
-    model = Post
-    form_class = PostForm
-    template_name = 'blog/post_form.html'
-    success_url = reverse_lazy('post-list')
+# -----------------------------
+# Create Comment
+# -----------------------------
+class CommentCreateView(LoginRequiredMixin, CreateView):
+    model = Comment
+    form_class = CommentForm
+    template_name = 'comment_form.html'
 
     def form_valid(self, form):
         form.instance.author = self.request.user
+        post_id = self.kwargs.get('post_id')
+        form.instance.post = get_object_or_404(Post, pk=post_id)
         return super().form_valid(form)
 
+    def get_success_url(self):
+        return self.object.post.get_absolute_url()  # Redirect to post detail
 
-class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
-    model = Post
-    form_class = PostForm
-    template_name = 'blog/post_form.html'
-    success_url = reverse_lazy('post-list')
-
-    def test_func(self):
-        post = self.get_object()
-        return self.request.user == post.author
-
-
-class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
-    model = Post
-    template_name = 'blog/post_confirm_delete.html'
-    success_url = reverse_lazy('post-list')
+# -----------------------------
+# Update Comment
+# -----------------------------
+class CommentUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Comment
+    form_class = CommentForm
+    template_name = 'comment_form.html'
 
     def test_func(self):
-        post = self.get_object()
-        return self.request.user == post.author
+        comment = self.get_object()
+        return self.request.user == comment.author
 
+    def get_success_url(self):
+        return self.object.post.get_absolute_url()
 
-# ---------------------
-# SEARCH VIEW
-# ---------------------
+# -----------------------------
+# Delete Comment
+# -----------------------------
+class CommentDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Comment
+    template_name = 'comment_confirm_delete.html'
 
-def search(request):
-    query = request.GET.get('q', '')  # get the search query
-    if query:
-        results = Post.objects.filter(
-            Q(title__icontains=query) | Q(content__icontains=query)
-        ).order_by('-published_date')
-    else:
-        results = Post.objects.none()  # no results if query is empty
+    def test_func(self):
+        comment = self.get_object()
+        return self.request.user == comment.author
 
-    return render(request, 'blog/search_results.html', {
-        'posts': results,
-        'query': query
-    })
+    def get_success_url(self):
+        return self.object.post.get_absolute_url()
