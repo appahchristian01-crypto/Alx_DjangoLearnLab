d00ab4c657bc49162ad2bfd0f7213dc7747e454c
appahchristian01-crypto <appahchristian01@gmail.com>
2025-12-06 05:31:12 -0800

    settings.py

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -79,10 +79,15 @@ WSGI_APPLICATION = 'django_blog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mydatabase',
+        'USER': 'myuser',
+        'PASSWORD': 'mypassword',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
