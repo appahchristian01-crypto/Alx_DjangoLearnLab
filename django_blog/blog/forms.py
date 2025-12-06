618eb82f31349ed6a1bcf4db0c2c18c2b1ee43b8
appahchristian01-crypto <appahchristian01@gmail.com>
2025-12-06 07:08:55 -0800

    forms.py

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from .models import Post, Comment
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from taggit.forms import TagWidget
 
 # -----------------------------
 # Post Form
@@ -30,3 +31,4 @@ class CommentForm(forms.ModelForm):
         content = self.cleaned_data.get('content')
         if not content or len(content.strip()) == 0:
             raise forms.ValidationError
+
