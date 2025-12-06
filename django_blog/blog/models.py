f42f6bd3c11d5c962a7cd55526745ef0aa50d535
appahchristian01-crypto <appahchristian01@gmail.com>
2025-12-06 07:20:18 -0800

    models.py

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -1,20 +1,15 @@
 from django.db import models
 from django.contrib.auth.models import User
-
-
-class Tag(models.Model):
-    name = models.CharField(max_length=50, unique=True)
-
-    def __str__(self):
-        return self.name
-
+from taggit.managers import TaggableManager
 
 class Post(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     content = models.TextField()
-    published_date = models.DateTimeField(auto_now_add=True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
-    tags = models.ManyToManyField(Tag, related_name='posts', blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    tags = TaggableManager()  # <-- tagging system
 
     def __str__(self):
         return self.title
@@ -28,4 +23,4 @@ class Comment(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return f'Comment by {self.author.username} on {self.post.title}'
+        return f"Comment by {self.author.username} on {self.post.title}"
