fdc017b57482a43a46629c23fd6b8afcedaf50e6
appahchristian01-crypto <appahchristian01@gmail.com>
2025-12-06 06:28:36 -0800

    views.py

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,23 +1,88 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.contrib.auth.models import User
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from .models import Post, Comment
+from .forms import PostForm, CommentForm, RegisterForm
 
+# -----------------------------
+# Post Views
+# -----------------------------
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'post_list.html'
+    context_object_name = 'posts'
+    ordering = ['-id']
+    paginate_by = 5
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'post_detail.html'
+    context_object_name = 'post'
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    form_class = PostForm
+    template_name = 'post_form.html'
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    form_class = PostForm
+    template_name = 'post_form.html'
+
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    template_name = 'post_confirm_delete.html'
+    success_url = '/'
+
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author
+
+# -----------------------------
+# User Registration
+# -----------------------------
+def register(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Account created successfully! You can now log in.')
+            return redirect('login')
+    else:
+        form = RegisterForm()
+    return render(request, 'register.html', {'form': form})
+
+# -----------------------------
+# Profile View
+# -----------------------------
 @login_required
 def profile(request):
-    user = request.user  # currently logged-in user
-
+    user = request.user
     if request.method == 'POST':
         username = request.POST.get('username')
         email = request.POST.get('email')
-
-        # Update user info
         user.username = username
         user.email = email
-        user.save()  # <-- This is required to persist changes
-
+        user.save()
         messages.success(request, "Your profile has been updated!")
-        return redirect('profile')  # Redirect to avoid re-submission
-
-    # GET request: show current user info
+        return redirect('profile')
     return render(request, 'profile.html', {'user': user})
+
+# -----------------------------
+# Search View
+# -----------------------------
+def search(request):
+    query = request.GET.get('q', '')
+    results = Post.objects.filter(title__
