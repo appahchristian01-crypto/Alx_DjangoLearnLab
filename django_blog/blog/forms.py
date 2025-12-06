51cae5df5821ed991afdf7e2328bf4e488a6f3f4
appahchristian01-crypto <appahchristian01@gmail.com>
2025-12-06 04:42:05 -0800

    Add files via upload

---
+++ django_blog/blog/forms.py
@@ -0,0 +1,20 @@
+from django import forms
+from .models import Post, Comment
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ['title', 'content']  # tags if you added them
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['content']
+
+class RegisterForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+    class Meta:
+        model = User
+        fields = ("username", "email", "password1", "password2")
