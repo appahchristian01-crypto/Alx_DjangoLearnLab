c2aae84470006794319b90840ae6e96e3a0dea03
appahchristian01-crypto <appahchristian01@gmail.com>
2025-12-06 04:40:40 -0800

    Delete django_blog directory

--- django_blog/blog/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from .models import Post
-
-def index(request):
-    posts = Post.objects.order_by('-published_date')
-    return render(request, 'blog/index.html', {'posts': posts})
